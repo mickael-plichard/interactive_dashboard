8d7ab96d748f6715425f074a574b941ca6c2f31a
Mickael Plichard <mickael.plichard@gmail.com>
2025-04-11 10:39:26 +0200

    Initial commit

---
+++ dashboard-backend/src/main/java/fr/studies/interactive_dashboard/InteractiveDashboardApplication.java
@@ -0,0 +1,13 @@
+package fr.studies.interactive_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveDashboardApplication.class, args);
+	}
+
+}
---
+++ dashboard-backend/src/test/java/fr/studies/interactive_dashboard/InteractiveDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.studies.interactive_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
